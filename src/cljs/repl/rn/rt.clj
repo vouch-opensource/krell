257c8334abcf0e816b55856859773597722d7914
dnolen <david.nolen@gmail.com>
2020-03-31 11:17:20 -0400

    can match react-native require

--- src/cljs/repl/rn/rt.clj
+++ src/cljs/repl/rn/rt.clj
@@ -1,8 +1,25 @@
 (ns cljs.repl.rn.rt
-  (:require [cljs.build.api :as api]))
+  (:require [cljs.build.api :as api]
+            [clojure.set :as set]))
 
-(defn npm-nses [main]
-  )
+(defn npm-nses
+  [opts]
+  (let [cljs-libs (-> (:main opts)
+                    api/compilable->ijs
+                    api/add-dependency-sources)
+        npm-libs  (api/index-ijs (api/node-modules opts))]
+    (set/intersection
+      ;; node lib names will be strings
+      (into #{} (mapcat #(map str %)) (map :requires cljs-libs))
+      (into #{} (keys npm-libs)))))
 
 (comment
+
+  (def opts
+    {:main 'hello-world.core
+     :target :nodejs
+     :npm-deps true})
+
+  (npm-nses opts)
+
   )
