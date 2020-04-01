75f09b9a7ccaaf0219dec2b7e444ca70e2eb5f90
dnolen <david.nolen@gmail.com>
2020-04-01 15:08:42 -0400

    add todo about -re validation

--- src/krell/main.clj
+++ src/krell/main.clj
@@ -1,1 +1,7 @@
-(ns krell.main)
+(ns krell.main
+  (:require [cljs.main]))
+
+;; TODO: check that either -re is already -re krell.repl or not
+;; supplied
+(defn -main [& args]
+  (apply cljs.main/-main (concat ["-re" "krell.repl"] args)))
