29aa8e2a98f0aa437797a744da3272bf97179b40
dnolen <david.nolen@gmail.com>
2020-04-02 18:56:29 -0400

    wait for core

--- resources/main.js
+++ resources/main.js
@@ -1,18 +1,25 @@
 import '$KRELL_OUTPUT_DIR/krell_repl.js';
 
+function waitForCore(cb) {
+    if(typeof cljs !== 'undefine') {
+        cb();
+    } else {
+        setTimeout(function() { waitForCore(cb); }, 250);
+    }
+}
+
 function krellUpdateRoot(cb) {
-    // REPL CASE
-    // wait for goog, base and core
+    waitForCore(function() {
+        // then load the main ns
+        // use debugloader to get the path from the name of the cs
+        // listen for the path to load
+        // invoke -main function in the main ns
+        // invoke cb
 
-    // then load the main ns
-    // use debugloader to get the path from the name of the cs
-    // listen for the path to load
-    // invoke -main function in the main ns
-    // invoke cb
-
-    // PROD CASE
-    // invoke -main function in the main ns
-    // invoke cb
+        // PROD CASE
+        // invoke -main function in the main ns
+        // invoke cb
+    });
 }
 
 module.exports = {
