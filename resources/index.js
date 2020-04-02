90264e77eb64a65cbd70e035fec411c2d8850d8d
dnolen <david.nolen@gmail.com>
2020-04-02 12:19:29 -0400

    main.js must provide krellSetBoostrapEl

--- resources/index.js
+++ resources/index.js
@@ -5,7 +5,7 @@
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
 import '$KRELL_OUTPUT_DIR/krell_repl.js';
-import '$KRELL_OUTPUT_TO';
+import {setBootstrapEl} from '$KRELL_OUTPUT_TO';
 
 var App = function () {
     return createReactClass({
