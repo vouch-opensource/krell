24530881cfbc5c19fff98f6de0bb73b7917b6437
dnolen <david.nolen@gmail.com>
2020-04-02 15:12:16 -0400

    enumerate a plan

--- resources/main.js
+++ resources/main.js
@@ -1,7 +1,18 @@
 import '$KRELL_OUTPUT_DIR/krell_repl.js';
 
 function krellUpdateRoot(cb) {
-    // wait for socket to be ready// then load the main ns
+    // REPL CASE
+    // wait for goog, base and core
+
+    // then load the main ns
+    // use debugloader to get the path from the name of the cs
+    // listen for the path to load
+    // invoke -main function in the main ns
+    // invoke cb
+
+    // PROD CASE
+    // invoke -main function in the main ns
+    // invoke cb
 }
 
 module.exports = {
