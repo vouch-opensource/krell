299281755d9a670c9a556485e6f2c113d82e77f9
dnolen <david.nolen@gmail.com>
2020-04-02 10:31:07 -0400

    write index wip

--- src/krell/gen.clj
+++ src/krell/gen.clj
@@ -1,8 +1,8 @@
 (ns krell.gen
   (:require [cljs.build.api :as api]
             [clojure.java.io :as io]
-            [clojure.string :as string]
-            [clojure.set :as set]))
+            [clojure.set :as set]
+            [clojure.string :as string]))
 
 (defn export-lib [lib]
   (str "\""lib "\": require('" lib "')" ))
@@ -31,15 +31,26 @@
         rt-js    (gen-rt-libs npm-deps)]
     (spit (io/file "rt.js") rt-js)))
 
+(defn write-index-js [opts]
+  (let [js (slurp (io/resource "index.j"))]
+    (spit (io/file "index.js")
+      (-> js
+        (string/replace "$KRELL_OUTPUT_TO" (:output-to opts))
+        (string/replace "$KRELL_OUTPUT_DIR" (:output-dir opts))))))
+
 (comment
 
   (def opts
     {:main 'hello-world.core
+     :output-to "out/main.js"
+     :output-dir "out"
      :target :nodejs
      :npm-deps true})
 
   (println (gen-rt-libs (npm-nses opts)))
 
   (gen-rt-js opts)
+
+  (write-index-js opts)
 
   )
