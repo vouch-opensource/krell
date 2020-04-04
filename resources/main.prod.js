beead115d192e132262d61d46708bd0bf91a60cb
dnolen <david.nolen@gmail.com>
2020-04-03 22:15:11 -0400

    copy over the helpers from the dev file - maybe we can share later - but the prod file is just appended to the output

--- resources/main.prod.js
+++ resources/main.prod.js
@@ -1,16 +1,28 @@
 var main = '$KRELL_MAIN_NS';
 
 function getIn(obj, xs) {
-    if(xs.length === 0) {
+    if(obj == null) {
+        return null;
+    } else if(xs.length === 0) {
         return obj;
     } else {
-        return getIn(obj[xs[0]], xs.slice(0));
+        return getIn(obj[xs[0]], xs.slice(1));
+    }
+}
+
+function getMainFn(ns) {
+    var xs = ns.split(".").concat(["_main"]),
+        fn = getIn(global, xs);
+    if(fn) {
+        return fn;
+    } else {
+        throw new Error("Could not find -main fn in namespace " + ns);
     }
 }
 
 function krellUpdateRoot(cb) {
-    var main = getIn(global, main.split(".").concat(["-main"]));
-    cb(main());
+    // TODO: won't work in :advanced w/o exporting
+    cb(getMainFn(main)());
 }
 
 module.exports = {
