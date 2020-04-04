c2857915885fe79a7958485b7399f3f53f5a34d8
dnolen <david.nolen@gmail.com>
2020-04-03 22:23:10 -0400

    fix prod main.js

--- resources/main.prod.js
+++ resources/main.prod.js
@@ -1,28 +1,6 @@
-var main = '$KRELL_MAIN_NS';
-
-function getIn(obj, xs) {
-    if(obj == null) {
-        return null;
-    } else if(xs.length === 0) {
-        return obj;
-    } else {
-        return getIn(obj[xs[0]], xs.slice(1));
-    }
-}
-
-function getMainFn(ns) {
-    var xs = ns.split(".").concat(["_main"]),
-        fn = getIn(global, xs);
-    if(fn) {
-        return fn;
-    } else {
-        throw new Error("Could not find -main fn in namespace " + ns);
-    }
-}
-
 function krellUpdateRoot(cb) {
     // TODO: won't work in :advanced w/o exporting
-    cb(getMainFn(main)());
+    cb($KRELL_MAIN_NS._main());
 }
 
 module.exports = {
