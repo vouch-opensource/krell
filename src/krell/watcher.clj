46076c0586505352896d3dfa05fb00af52ec881e
dnolen <david.nolen@gmail.com>
2020-04-09 10:50:48 -0400

    add path helper

--- src/krell/watcher.clj
+++ src/krell/watcher.clj
@@ -1,4 +1,16 @@
 (ns krell.watcher
+  (:require [clojure.java.io :as io])
   (:import [java.io IOException]
-           [io.methvin.watcher DirectoryWatcher]
-           ))
+           [java.nio.file Paths]
+           [io.methvin.watcher DirectoryWatcher]))
+
+(defn to-path [& args]
+  (Paths/get (first args) (into-array String (rest args))))
+
+(comment
+
+  (-> (DirectoryWatcher/builder)
+    (.path (to-path "src"))
+    )
+
+  )
