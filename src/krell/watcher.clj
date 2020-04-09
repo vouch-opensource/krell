91f561e158642b0d1af63d0f6d683a0999ae63b3
dnolen <david.nolen@gmail.com>
2020-04-09 13:02:18 -0400

    cleanup watcher ns to be more useful

--- src/krell/watcher.clj
+++ src/krell/watcher.clj
@@ -1,34 +1,30 @@
 (ns krell.watcher
   (:require [clojure.java.io :as io])
-  (:import [io.methvin.watcher DirectoryChangeEvent DirectoryChangeListener
-                               DirectoryWatcher]
+  (:import [io.methvin.watcher DirectoryChangeEvent DirectoryChangeEvent$EventType
+                               DirectoryChangeListener DirectoryWatcher]
            [java.nio.file Paths]
            [org.slf4j LoggerFactory]))
 
 (def logger (LoggerFactory/getLogger "krell"))
 
-(defn f->l [f]
+(defn fn->listener ^DirectoryChangeListener [f]
   (reify
     DirectoryChangeListener
     (onEvent [this e]
-      (f e))))
-
-(defn listener* [^DirectoryChangeEvent e]
-  (. logger (info (str "EVENT " (. e eventType))))
-  (case (. e eventType)
-    DirectoryChangeEvent/CREATE (. logger (info "CREATE"))
-    DirectoryChangeEvent/MODIFY (. logger (info "MODIFY"))
-    DirectoryChangeEvent/DELETE (. logger (info "DELETE"))))
-
-(def listener (f->l listener*))
+      (let [path (.path ^DirectoryChangeEvent e)]
+        (condp = (. ^DirectoryChangeEvent e eventType)
+          DirectoryChangeEvent$EventType/CREATE   (f {:type :create :path path})
+          DirectoryChangeEvent$EventType/MODIFY   (f {:type :modify :path path})
+          DirectoryChangeEvent$EventType/DELETE   (f {:type :delete :path path})
+          DirectoryChangeEvent$EventType/OVERFLOW (f {:type :overflow :path path}))))))
 
 (defn to-path [& args]
-  (Paths/get (first args) (into-array String (rest args))))
+  (Paths/get ^String (first args) (into-array String (rest args))))
 
-(defn create [& paths]
+(defn create [cb & paths]
   (-> (DirectoryWatcher/builder)
     (.paths (map to-path paths))
-    (.listener listener)
+    (.listener (fn->listener cb))
     (.build)))
 
 (defn watch [^DirectoryWatcher watcher]
@@ -39,8 +35,13 @@
 
 (comment
 
-  (def watcher (create "src"))
+  (def watcher
+    (create
+      (fn [e]
+        (. logger (info (pr-str e))))
+      "src"))
 
   (watch watcher)
 
   )
+
