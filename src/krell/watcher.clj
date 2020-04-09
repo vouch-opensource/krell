67598d0ed829366a96abefae360dc6dc67d03af6
dnolen <david.nolen@gmail.com>
2020-04-09 11:41:28 -0400

    wip

--- src/krell/watcher.clj
+++ src/krell/watcher.clj
@@ -2,7 +2,22 @@
   (:require [clojure.java.io :as io])
   (:import [java.io IOException]
            [java.nio.file Paths]
-           [io.methvin.watcher DirectoryWatcher]))
+           [io.methvin.watcher DirectoryChangeEvent DirectoryChangeListener
+                               DirectoryWatcher]))
+
+(defn f->l [f]
+  (reify
+    DirectoryChangeListener
+    (onEvent [this e]
+      (f e))))
+
+(defn listener* [^DirectoryChangeEvent e]
+  (case (. e eventType)
+    DirectoryChangeEvent/CREATE (println "CREATE")
+    DirectoryChangeEvent/MODIFY (println "MODIFY")
+    DirectoryChangeEvent/DELETE (println "DELETE")))
+
+(def listener (f->l listener*))
 
 (defn to-path [& args]
   (Paths/get (first args) (into-array String (rest args))))
@@ -11,6 +26,8 @@
 
   (-> (DirectoryWatcher/builder)
     (.path (to-path "src"))
-    )
+    (.listener listener)
+    (.build))
+
 
   )
