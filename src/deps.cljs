57b604a0698d706c87f26e8f729f391dc6fcc55d
dnolen <david.nolen@gmail.com>
2020-05-07 13:45:45 -0400

    add sync-storage dep

--- src/deps.cljs
+++ src/deps.cljs
@@ -1,2 +1,3 @@
-{:npm-deps {"react-native-tcp-socket"  "3.5.0"
+{:npm-deps {"sync-storage"             "0.4.2"
+            "react-native-tcp-socket"  "3.5.0"
             "react-native-zeroconf"    "0.11.6"}}
