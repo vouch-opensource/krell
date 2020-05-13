77847702071a54b04af75f2cfe0953dbd8d1a7b4
dnolen <david.nolen@gmail.com>
2020-05-13 09:38:15 -0400

    bump rn-tcp-socket

--- src/deps.cljs
+++ src/deps.cljs
@@ -1,2 +1,2 @@
 {:npm-deps {"@react-native-community/async-storage" "1.10.0"
-            "react-native-tcp-socket"               "3.5.0"}}
+            "react-native-tcp-socket"               "3.6.0"}}
