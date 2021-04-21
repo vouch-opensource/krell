658b9dd770e1b652d5361bea272db8ead6dbf3a9
davidnolen <david.nolen@gmail.com>
2021-04-21 10:19:11 -0400

    (wip) fix async storage org

--- src/deps.cljs
+++ src/deps.cljs
@@ -1,2 +1,2 @@
-{:npm-deps {"@react-native-community/async-storage" "1.15.3"
-            "react-native-tcp-socket"               "5.2.0"}}
+{:npm-deps {"@react-native-async-storage/async-storage" "1.15.3"
+            "react-native-tcp-socket"                   "5.2.0"}}
